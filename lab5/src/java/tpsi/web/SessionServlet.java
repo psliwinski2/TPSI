61dceab19358d68643dd17aa4d108b70b9de8f2b
psliwinski2 <63249645+psliwinski2@users.noreply.github.com>
2020-04-20 16:35:29 +0200

    Add files via upload

---
+++ lab5/src/java/tpsi/web/SessionServlet.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpsi.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Lenovo
+ */
+@WebServlet(name = "SessionServlet", urlPatterns = {"/session"})
+public class SessionServlet extends HttpServlet {
+
+    private int visitCount;
+    private List<Student> studenci = new ArrayList<>();
+
+    public void init() {
+        visitCount = 0;
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        visitCount++;
+
+        session.setAttribute("listaStudentow", studenci);
+        session.setAttribute("wizyty", visitCount);
+        request.getRequestDispatcher("session.jsp").forward(request, response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        studenci.add(new Student(request.getParameter("firstName"), request.getParameter("lastName"), request.getParameter("email")));
+        response.sendRedirect("/lab5/session");
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }//
+
+}
